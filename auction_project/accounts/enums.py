cb66c97ac9daaa0932aecb2031c1842ae3a6c753
YovchoGandjurov <yovchoo@gmail.com>
2019-05-17 14:43:59 +0300

    Added extended user as Profile

---
+++ auction_project/accounts/enums.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Languages(Enum):
+    BG = 'Български'
+    EN = 'English'
+    ES = 'Español'
+    IT = 'Italiano'
+    DE = 'Deutsch'
+    FR = 'Français'
--- auction_project/accounts/models.py
+++ auction_project/accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_delete, post_save
 
-# Create your models here.
+from .enums import Languages
+
+
+user_img = 'https://www.compassdigitalskills.eu/img/generic/generic-user.png'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='profile')
+    first_last_name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=254)
+    profile_image = models.URLField(default=user_img)
+    language = models.CharField(max_length=50,
+                                choices=[(l.name, l.value) for l in Languages])
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def delete(self, *args, **kwargs):
+        self.user.delete()
+        return super(self.__class__, self).delete(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.user}'
