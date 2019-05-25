d8769810deefd3125f2b0026f97cca3240ad6611
YovchoGandjurov <yovchoo@gmail.com>
2019-05-25 19:09:18 +0300

    Updated account tests

--- auction_project/accounts/tests.py
+++ auction_project/accounts/tests.py
@@ -5,22 +5,18 @@ from django.contrib.auth.models import User
 
 from .models import Profile
 
-"""
-Register testing is not working. TODO - update to work
 
 class RegisterTestCase(APITestCase):
     url = '/accounts/register/'
 
     def setUp(self):
         self.data = {
-            "user": {
-                "username": "user_test",
-                "password": "password_test",
-                "first_name": "name_test",
-                "last_name": "last_name_test",
-                "email": "test@test.te"
-            },
-            "language": "English"
+            "username": "TestCase_user",
+            "password": "123",
+            "email": "test@test.te",
+            "first_name": "test",
+            "last_name": "testov",
+            "language": "BG",
         }
         self.client = APIClient()
 
@@ -29,7 +25,6 @@ class RegisterTestCase(APITestCase):
         response = self.client.post(self.url, data=self.data,
                                     format='json')
         after_save = Profile.objects.count()
-        # import ipdb; ipdb.set_trace()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED,
                          'Expected status code 201, got {}'.format(
                              response.status_code))
@@ -40,16 +35,16 @@ class RegisterTestCase(APITestCase):
     def test_password_not_in_response(self):
         response = self.client.post(self.url, data=self.data,
                                     format='json')
-        # import ipdb; ipdb.set_trace()
         self.assertTrue('password' not in response.data,
                         'Password should not be in the response body.')
 
     def test_password_storing_in_plaintext(self):
+
         response = self.client.post(self.url, data=self.data,
                                     format='json')
         profile = Profile.objects.first()
         self.assertNotEqual(profile.user.password,
-                            self.data['user']['password'],
+                            self.data['password'],
                             'Password shoild not be in plaintext')
 
 
@@ -60,14 +55,20 @@ class LoginTestCase(APITestCase):
         self.client = APIClient()
 
         self.user_data = {
-            "username": "user_test",
-            "password": "password_test",
-            "first_name": "name_test",
-            "last_name": "last_name_test",
-            "email": "test@test.te"
-            }
+            "username": "TestCase_user",
+            "password": "123",
+            "email": "test@test.te",
+            "first_name": "test",
+            "last_name": "testov"
+        }
         self.profile_data = {"language": "BG"}
-        
+
         self.user = User.objects.create(**self.user_data)
-        self.profile = Profile.objects.create(user=user, **self.profile_data)
-"""
+        self.profile = Profile.objects.create(
+                user=self.user, **self.profile_data)
+
+    def test_login_valid_credentials(self):
+        self.client.force_authenticate(self.user)
+        profile = Profile.objects.first()
+        get_result = self.client.get('/accounts/' + str(self.user.id) + '/')
+        self.assertEqual(get_result.status_code, status.HTTP_200_OK)
